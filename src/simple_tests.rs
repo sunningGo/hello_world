26b7e28321c78282ca3cc4a3bd737d30353880f9
Sunning Go <sunning_go@hotmail.com>
2023-10-11 02:58:38 -0400

    Changes in simple_tests.rs

--- src/simple_tests.rs
+++ src/simple_tests.rs
@@ -1,21 +1,14 @@
 #![allow(unused)]
 
 
-fn balance_factor(x: u8, y: u8) -> i8 {
-    let diff = x as i16 - y as i16;
-    debug_assert!(-2 <= diff && diff <= 2);
-    diff as i8
-}
-
-fn balance_factor2(x: u8, y: u8) -> i8 {
-    let diff = x - y;
-    0
+fn f() {
+    let mut v = vec![];
+    v.push(0);
 }
 
 pub fn main() {
     println!("Hello from simple_tests");
 
-    println!("{}", balance_factor(1, 3));
 
     println!("Bye");
 }
